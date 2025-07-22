f86e6b8ee77322921d15dc146b6f41d9bc32d2c5
Prateek Mohapatra <prateekmsoa@gmail.com>
2025-07-23 03:11:29 +0530

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+export default defineConfig(({ mode }) => {
+  // Load env vars
+  const env = loadEnv(mode, process.cwd(), "");
+  const backendUrl = env.VITE_API_URL || "http://localhost:8000";
+  return {
+    server: {
+      host: "::",
+      port: 8080,
+      proxy: {
+        "/files": backendUrl,
+      },
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
